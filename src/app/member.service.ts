b10453273b91ea02cbe889fb4dfd072def4eb7e5
Philip Putnam <putnamp989@gmail.com>
2017-04-07 09:49:44 -0700

    add member.service with Firebase connection and data injected to members route

---
+++ src/app/member.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Injectable()
+export class MemberService {
+  members: FirebaseListObservable<any[]>;
+
+  constructor(private angularFire: AngularFire) {
+    this.members = angularFire.database.list('members');
+  }
+
+  getMembers() {
+    return this.members;
+  }
+
+}
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../member.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls: ['./members.component.css'],
+  providers: [MemberService]
 })
 export class MembersComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private memberService: MemberService) {}
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
 }
