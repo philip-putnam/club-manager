007fc95e8582365f9aa6701c48443e9297a7da5b
Philip Putnam <putnamp989@gmail.com>
2017-04-07 10:37:01 -0700

    display member details on dynamic profile pages

--- src/app/member-profile/member-profile.component.ts
+++ src/app/member-profile/member-profile.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-member-profile',
   templateUrl: './member-profile.component.html',
-  styleUrls: ['./member-profile.component.css']
+  styleUrls: ['./member-profile.component.css'],
+  providers: [MemberService]
 })
 export class MemberProfileComponent implements OnInit {
+  member;
+  memberId: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.memberId = urlParameters['id'];
+    });
+    this.member = this.memberService.getMemberById(this.memberId);
   }
 
 }
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -13,4 +13,8 @@ export class MemberService {
     return this.members;
   }
 
+  getMemberById(memberId: string) {
+    return this.angularFire.database.object('members/' + memberId);
+  }
+
 }
